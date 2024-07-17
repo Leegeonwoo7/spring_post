5f43c2371e9bb51d720ee85fab2ed75235ed97ee
Leegeonwoo7 <workgeon07@naver.com>
2024-07-18 02:24:51 +0900

    test: Member Service 테스트코드

--- src/test/java/com/post/service/member/MemberServiceTest.java
+++ src/test/java/com/post/service/member/MemberServiceTest.java
@@ -3,8 +3,11 @@ package com.post.service.member;
 import com.post.domain.member.Address;
 import com.post.domain.member.Member;
 import com.post.exception.ExistMemberEmailException;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.DisplayName;
+import com.post.exception.ExistMemberLoginIdException;
+import com.post.exception.ExistMemberNameException;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,13 +20,18 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
 class MemberServiceTest {
 
     @Autowired
     MemberService memberService;
 
+    @Autowired
+    EntityManager em;
+
     @Test
     @DisplayName("중복되지 않는 이메일, 아이디로 회원가입시 회원가입에 성공한다")
+    @Order(1)
     @Transactional
     void join() {
         // given
@@ -39,6 +47,7 @@ class MemberServiceTest {
 
     @Test
     @DisplayName("중복되는 이메일로 회원가입시 회원가입에 실패한다.")
+    @Order(2)
     @Transactional
     void duplicateEmailJoinMember() {
         // given
@@ -51,6 +60,140 @@ class MemberServiceTest {
         //then
         assertThatThrownBy(() -> memberService.join(duplicateMember))
                 .isInstanceOf(ExistMemberEmailException.class);
+    }
+
+    @Test
+    @DisplayName("중복되는 아이디로 회원가입시 회원가입에 실패한다")
+    @Order(3)
+    @Transactional
+    void duplicateLoginIdJoinMember() {
+        // given
+        Member memberA = Member.builder()
+                .loginId("memberA")
+                .password("1234")
+                .name("멤버A")
+                .build();
+        memberService.join(memberA);
+
+        Member memberB = Member.builder()
+                .loginId("memberA")
+                .password("4567")
+                .name("멤버B")
+                .build();
+
+        //when
+        assertThatThrownBy(() -> memberService.join(memberB))
+                .isInstanceOf(ExistMemberLoginIdException.class);
+
+        //then
+        assertThat(memberA.getLoginId()).isEqualTo(memberB.getLoginId());
+        assertThat(memberA).isNotEqualTo(memberB);
+    }
+
+    @Test
+    @DisplayName("name이 NULL이면 loginId가 name으로 저장된다")
+    @Order(4)
+    @Transactional
+    void nameNullJoin() {
+        // given
+        Member memberA = Member.builder()
+                .password("1234")
+                .loginId("memberA")
+                .build();
+
+        //when
+        Member joinMember = memberService.join(memberA);
+        Member findMember = memberService.findMemberById(joinMember.getId());
+
+        //then
+        assertThat(findMember.getName()).isEqualTo("memberA");
+    }
+
+    @Test
+    @DisplayName("loginId와 name이 이미 존재하면 예외가 발생한다 ")
+    @Order(4)
+    @Transactional
+    void duplicateLoginIdNameJoin() {
+        // given
+        Member member = Member.builder()
+                .password("1234")
+                .loginId("memberA")
+                .name("회원A")
+                .build();
+
+        Member duplicateMember = Member.builder()
+                .password("5678")
+                .loginId("memberB")
+                .name("memberA")
+                .build();
+
+        memberService.join(member);
+
+        //when
+        //then
+        assertThatThrownBy(() -> memberService.join(duplicateMember))
+                .isInstanceOf(ExistMemberNameException.class);
+
+    }
+
+    @Test
+    @DisplayName("id필드로 회원을 조회한다")
+    @Order(5)
+    @Transactional
+    void findMemberById() {
+        // given
+        Member memberA = Member.builder()
+                .loginId("memberA")
+                .password("1234")
+                .build();
+        Member joinMember = memberService.join(memberA);
+
+        //when
+        Member memberById = memberService.findMemberById(joinMember.getId());
+
+        //then
+        assertThat(joinMember).isEqualTo(memberById);
+    }
+
+    @Test
+    @DisplayName("비밀번호를 입력된 값으로 변경한다")
+    @Order(6)
+    @Transactional
+    void changePassword() {
+        // given
+        Member member = createMember();
+        Member saveMember = memberService.join(member);
+
+        String newPassword = "newPassword";
+
+        //when
+        memberService.changePassword(saveMember.getId(), newPassword);
+        Member updateMember = memberService.findMemberById(saveMember.getId());
+
+        //then
+        assertThat(updateMember.getPassword()).isEqualTo(newPassword);
+    }
+
+    @Test
+    @DisplayName("회원이름을 입력된 값으로 변경한다")
+    @Order(7)
+    @Transactional
+    void changeName() {
+        // given
+        Member memberA = Member.builder()
+                .loginId("memberA")
+                .password("1234")
+                .build();
+        Member joinMember = memberService.join(memberA);
+
+        String newName = "newName";
+
+        //when
+        memberService.changeName(joinMember.getId(), newName);
+        Member updateMember = memberService.findMemberById(joinMember.getId());
+
+        //then
+        assertThat(updateMember.getName()).isEqualTo(newName);
     }
 
     private Member createMember() {
