e779d226b7de0b08d5f453f926493dece4fcc078
Leegeonwoo7 <workgeon07@naver.com>
2024-07-18 20:20:57 +0900

    feat: DTO Builder구현

--- src/main/java/com/post/api/member/response/MemberResponse.java
+++ src/main/java/com/post/api/member/response/MemberResponse.java
@@ -1,22 +1,64 @@
 package com.post.api.member.response;
 
 import com.post.domain.member.Address;
+import com.post.domain.member.Member;
 import com.post.domain.member.Role;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Getter
-@Setter
+@AllArgsConstructor
 public class MemberResponse {
 
+    private Long id;
     private String loginId;
+    private String password;
     private String name;
     private String email;
     private Address address;
     private LocalDate birth;
     private String phone;
     private Role role;
-    private LocalDate createAt;
+    private LocalDateTime createAt;
+
+    public static MemberResponse from(Member member) {
+        return new MemberResponse(
+                member.getId(),
+                member.getLoginId(),
+                member.getPassword(),
+                member.getName(),
+                member.getEmail(),
+                member.getAddress(),
+                member.getBirthdate(),
+                member.getPhone(),
+                member.getRole(),
+                member.getCreateAt()
+        );
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object)
+            return true;
+        if (object == null || getClass() != object.getClass())
+            return false;
+        MemberResponse that = (MemberResponse) object;
+        return Objects.equals(getId(), that.getId()) &&
+                Objects.equals(getLoginId(), that.getLoginId()) &&
+                Objects.equals(getName(), that.getName()) &&
+                Objects.equals(getEmail(), that.getEmail()) &&
+                Objects.equals(getAddress(), that.getAddress()) &&
+                Objects.equals(getBirth(), that.getBirth()) &&
+                Objects.equals(getPhone(), that.getPhone()) && getRole() == that.getRole() &&
+                Objects.equals(getCreateAt(), that.getCreateAt());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getLoginId(), getName(), getEmail(), getAddress(), getBirth(), getPhone(), getRole(), getCreateAt());
+    }
 }
