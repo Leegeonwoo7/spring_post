6a9c0bf340a44f81e3d69ace9daffce891ff6ef6
Leegeonwoo7 <workgeon07@naver.com>
2024-07-25 04:06:46 +0900

    test: 검색조건 조회 테스트 구현

--- src/test/java/com/post/repository/board/BoardRepositoryImplTest.java
+++ src/test/java/com/post/repository/board/BoardRepositoryImplTest.java
@@ -1,11 +1,14 @@
 package com.post.repository.board;
 
+import com.post.api.board.request.SearchCond;
 import com.post.domain.board.Board;
 import com.post.domain.member.Member;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -75,6 +78,56 @@ class BoardRepositoryImplTest {
         assertThat(saveBoard).isEqualTo(findBoard);
         assertThat(saveBoard.getMember()).isEqualTo(findBoard.getMember());
         assertThat(saveBoard.getTitle()).isEqualTo(findBoard.getTitle());
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("게시글 제목으로 글 단건 조회")
+    void findByCondition() {
+        //given
+        boardRepository.save(boardA);
+
+        SearchCond cond = SearchCond.builder()
+                .title("A")
+                .build();
+
+        //when
+        List<Board> boardList = boardRepository.findByCondition(cond);
+
+        //then
+        assertThat(boardList).hasSize(1);
+    }
+
+    @Test
+    @DisplayName("게시글 제목으로 조회할 때 여러건을 조회할 수 있어야한다")
+    void findByConditionMultiple() {
+        // given
+        Board boardC = Board.builder()
+                .member(memberA)
+                .content("이 글은 게시글 C입니다.")
+                .title("C 게시글 입니다.")
+                .build();
+
+        Board boardD = Board.builder()
+                .member(memberA)
+                .content("이 글은 게시글 D입니다.")
+                .title("게시글D")
+                .build();
+
+        boardRepository.save(boardA);
+        boardRepository.save(boardB);
+        boardRepository.save(boardC);
+        boardRepository.save(boardD);
+
+        SearchCond cond = SearchCond.builder()
+                .title("게시글")
+                .build();
+        //when
+        List<Board> boardList = boardRepository.findByCondition(cond);
+
+        //then
+        assertThat(boardList).hasSize(4);
+        boardList.forEach(board -> assertThat(board.getTitle()).contains("게시글"));
     }
 
 }
