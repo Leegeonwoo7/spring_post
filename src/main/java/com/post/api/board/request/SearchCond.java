3fb47be419b0e42f6cb5f44deebdfce9707af758
Leegeonwoo7 <workgeon07@naver.com>
2024-07-25 03:52:48 +0900

    feat: 게시글 조회 검색조건 추가

--- src/main/java/com/post/api/board/request/SearchCond.java
+++ src/main/java/com/post/api/board/request/SearchCond.java
@@ -1,9 +1,15 @@
 package com.post.api.board.request;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class SearchCond {
 
     private String title;
+
+    @Builder
+    private SearchCond(String title) {
+        this.title = title;
+    }
 }
