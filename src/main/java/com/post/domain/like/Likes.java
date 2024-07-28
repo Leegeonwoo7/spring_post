bcc0c1fc1062e59a6c081df79a5dade57f4021a2
Leegeonwoo7 <workgeon07@naver.com>
2024-07-28 22:21:54 +0900

    feat: Like엔티티 기능추가

--- src/main/java/com/post/domain/like/Likes.java
+++ src/main/java/com/post/domain/like/Likes.java
@@ -3,12 +3,16 @@ package com.post.domain.like;
 import com.post.domain.board.Board;
 import com.post.domain.member.Member;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Likes {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,10 +20,19 @@ public class Likes {
     private Long id;
 
     @ManyToOne
+    @JoinColumn(name = "member_id")
     private Member member;
 
     @ManyToOne
+    @JoinColumn(name = "board_id")
     private Board board;
 
     private LocalDateTime createAt;
+
+    @Builder
+    private Likes(Member member, Board board, LocalDateTime createAt) {
+        this.member = member;
+        this.board = board;
+        this.createAt = createAt;
+    }
 }
