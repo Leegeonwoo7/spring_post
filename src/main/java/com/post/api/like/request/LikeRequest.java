92f6ad1275de88d350931ef7e0d676f2d42f8c63
Leegeonwoo7 <workgeon07@naver.com>
2024-07-28 22:24:08 +0900

    feat: 요청/응답객체

--- src/main/java/com/post/api/like/request/LikeRequest.java
+++ src/main/java/com/post/api/like/request/LikeRequest.java
@@ -1,2 +1,21 @@
-package com.post.api.like.request;public class LikeRequest {
+package com.post.api.like.request;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class LikeRequest {
+
+    private Long memberId;
+    private Long boardId;
+    private LocalDateTime createAt;
+
+    @Builder
+    private LikeRequest(Long memberId, Long boardId, LocalDateTime createAt) {
+        this.memberId = memberId;
+        this.boardId = boardId;
+        this.createAt = createAt;
+    }
 }
--- src/main/java/com/post/api/like/response/LikeResponse.java
+++ src/main/java/com/post/api/like/response/LikeResponse.java
@@ -1,2 +1,30 @@
-package com.post.api.like.response;public class LikeResponse {
+package com.post.api.like.response;
+
+import com.post.domain.like.Likes;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@EqualsAndHashCode
+public class LikeResponse {
+
+    private Long likeId;
+    private Long memberId;
+    private Long boardId;
+
+    @Builder
+    private LikeResponse(Long likeId, Long memberId, Long boardId) {
+        this.likeId = likeId;
+        this.memberId = memberId;
+        this.boardId = boardId;
+    }
+
+    public LikeResponse from(Likes likes) {
+        return new LikeResponse(
+                likes.getId(),
+                likes.getMember().getId(),
+                likes.getBoard().getId()
+        );
+    }
 }
