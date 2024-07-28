596686e224d2218dea2da83f48b289045b895afa
Leegeonwoo7 <workgeon07@naver.com>
2024-07-28 22:22:43 +0900

    feat: Like Repository계층 구현

--- src/main/java/com/post/repository/likes/LikeRepositoryImpl.java
+++ src/main/java/com/post/repository/likes/LikeRepositoryImpl.java
@@ -1,2 +1,42 @@
-package com.post.repository.likes;public class LikeRepositoryImpl {
+package com.post.repository.likes;
+
+import com.post.domain.like.Likes;
+import com.post.domain.member.Member;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Slf4j
+@RequiredArgsConstructor
+public class LikeRepositoryImpl implements LikeRepository{
+
+    private final EntityManager em;
+
+    @Override
+    public List<Member> findMembersByLike(Long boardId) {
+        String jpql = "select l.member from Likes l where l.board.id = :boardId";
+        return em.createQuery(jpql, Member.class)
+                .setParameter("boardId", boardId)
+                .getResultList();
+    }
+
+    @Override
+    public Long findLikeCount(Long boardId) {
+        String jpql = "select count(l) from Likes l where l.board.id = :boardId";
+        return em.createQuery(jpql, Long.class)
+                .setParameter("boardId", boardId)
+                .getSingleResult();
+    }
+
+    @Override
+    public Likes save(Likes likes) {
+        em.persist(likes);
+        return likes;
+    }
+
+
 }
