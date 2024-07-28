78f3a14fa59ad71ba53207671d65392ae1adc017
Leegeonwoo7 <workgeon07@naver.com>
2024-07-28 22:23:47 +0900

    feat: 좋아요를 누른 회원 조회 Response

--- src/main/java/com/post/api/like/response/MembersWithLikeResponse.java
+++ src/main/java/com/post/api/like/response/MembersWithLikeResponse.java
@@ -1,2 +1,25 @@
-package com.post.api.like.response;public class MembersWithLikeResponse {
+package com.post.api.like.response;
+
+import com.post.domain.member.Member;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class MembersWithLikeResponse {
+
+    private Long memberId;
+    private String name;
+
+    @Builder
+    private MembersWithLikeResponse(Long memberId, String name) {
+        this.memberId = memberId;
+        this.name = name;
+    }
+
+    public static MembersWithLikeResponse from(Member member) {
+        return new MembersWithLikeResponse(
+                member.getId(),
+                member.getLoginId()
+        );
+    }
 }
